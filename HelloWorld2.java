3110b167c6d16bf460bbab6a31035d87f03027d8
woehrlea <79633349+woehrlea@users.noreply.github.com>
2021-03-02 19:08:31 +0100

    Update HelloWorld2.java
    Erweiterung des Ausgabetextes.

--- HelloWorld2.java
+++ HelloWorld2.java
@@ -2,7 +2,7 @@ public class HelloWorld2 {
 
 	public static void main(String[] args) {
 		//Prints "Hello, World the second" to the terminal window.
-		System.out.println("Hello World");
+		System.out.println("Hello World und auch hier eine Änderung");
 	}
 }
 
